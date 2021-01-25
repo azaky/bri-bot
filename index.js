2a4c8fed16a8649a6ddf9bb02e937bbf7873bad3
Ahmad Zaky <ahmadzaky003@gmail.com>
2021-01-25 22:29:32 +0700

    notify on init

--- index.js
+++ index.js
@@ -25,8 +25,6 @@ if (process.env.DISCORD_TOKEN && process.env.DISCORD_USER_ID) {
   discord.on('message', message => {
     console.log('got message from', message.author.id, message.content);
   });
-
-  // discord.send()
 }
 
 const notify = async content => {
@@ -53,6 +51,7 @@ let scoreboards = [];
 let last = null;
 let lasttop10 = null;
 const contests = ['People Analytics', 'Cash Ratio Optimization'];
+const teamname = 'K2IV';
 
 const updateScore = async () => {
   try {
@@ -115,67 +114,67 @@ const updateScore = async () => {
       )
     );
 
-    const ours = scoreboards.map(scoreboard => scoreboard.find(team => team.name === 'K2IV'));
+    const ours = scoreboards.map(scoreboard => scoreboard.find(team => team.name === teamname));
     console.log(ours);
 
     // perhaps there should be a better way to do this
     const serialized = JSON.stringify(ours);
     if (JSON.stringify(last) !== serialized) {
-      if (last) {
         const updates = ours.map((result, i) => {
-          const changes = [];
+        const changes = [];
+        if (last) {
           if (result.rank < last[i].rank) {
-            changes.push(`You moved up the leaderboard from **rank ${last[i].rank}** to **rank ${result.rank}**!`);
+            changes.push(`Team ${teamname} moved up the leaderboard from **rank ${last[i].rank}** to **rank ${result.rank}**!`);
           }
           if (result.rank > last[i].rank) {
-            changes.push(`You moved down the leaderboard from **rank ${last[i].rank}** to **rank ${result.rank}** 😔`);
+            changes.push(`Team ${teamname} moved down the leaderboard from **rank ${last[i].rank}** to **rank ${result.rank}** 😔`);
           }
           if (result.score > last[i].score) {
-            changes.push(`Your score improved from **${last[i].score}** to **${result.score}**!`);
+            changes.push(`Team ${teamname}'s score improved from **${last[i].score}** to **${result.score}**!`);
           }
           if (result.score < last[i].score) {
-            changes.push(`Your score decreased from **${last[i].score}** to **${result.score}** ... but ... how ...?`);
+            changes.push(`Team ${teamname}'s score decreased from **${last[i].score}** to **${result.score}** ... but ... how ...?`);
           }
-          if (changes.length > 0) {
-            changes.push(`\`Submission Date: ${result.timestamp}\``);
-          }
-          return {
-            name: contests[i],
-            value: changes.join('\n'),
-          };
-        });
-        const message = new Discord.MessageEmbed()
-          .setColor('#008891')
-          .setTitle('Score/Rank Update!')
-          .setThumbnail('https://brihackathon.id/images/logo-bri-hackathon.png')
-          .setDescription('');
-        updates.forEach(update => {
-          if (update.value) {
-            message.addField(update.name, update.value);
-          }
-        });
-        await notify(message);
-      }
+        } else {
+          changes.push(`Team ${teamname} is on **rank ${result.rank}** of ${scoreboards[i].length} with score **${result.score}**`);
+        }
+        if (changes.length > 0) {
+          changes.push(`> Submission Date: ${result.timestamp}`);
+        }
+        return {
+          name: contests[i],
+          value: changes.join('\n'),
+        };
+      });
+      const message = new Discord.MessageEmbed()
+        .setColor('#008891')
+        .setTitle('Rank Notification')
+        .setThumbnail('https://brihackathon.id/images/logo-bri-hackathon.png')
+        .setDescription('');
+      updates.forEach(update => {
+        if (update.value) {
+          message.addField(update.name, update.value);
+        }
+      });
+      await notify(message);
       last = ours;
     }
 
     const top10 = scoreboards.map(scoreboard => scoreboard.slice(0, 10));
     const top10serialized = JSON.stringify(top10);
     if (top10serialized !== JSON.stringify(lasttop10)) {
-      if (lasttop10) {
-        const message = new Discord.MessageEmbed()
-          .setColor('#008891')
-          .setTitle('Top 10 Update!')
-          .setThumbnail('https://brihackathon.id/images/logo-bri-hackathon.png')
-          .setDescription('');
-        contests.forEach((title, i) => {
-          const table = new AsciiTable()
-          table.setHeading('Rank', 'Team Name', 'Score');
-          top10[i].forEach((team, rank) => table.addRow(rank+1, team.name, team.score));
-          message.addField(title, ['```', table.toString(), '```'].join('\n'));
-        });
-        await notify(message);
-      }
+      const message = new Discord.MessageEmbed()
+        .setColor('#008891')
+        .setTitle('Top 10 Leaderboard')
+        .setThumbnail('https://brihackathon.id/images/logo-bri-hackathon.png')
+        .setDescription('');
+      contests.forEach((title, i) => {
+        const table = new AsciiTable()
+        table.setHeading('Rank', 'Team Name', 'Score');
+        top10[i].forEach((team, rank) => table.addRow(rank+1, team.name, team.score));
+        message.addField(title, ['```', table.toString(), '```'].join('\n'));
+      });
+      await notify(message);
       lasttop10 = top10;
     }
   } catch (e) {
