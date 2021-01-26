c2eed5aab2f558fabdd4b14a46438fe07f36eabe
Ahmad Zaky <ahmadzaky003@gmail.com>
2021-01-26 13:11:03 +0700

    add changes to scoreboard

--- index.js
+++ index.js
@@ -119,6 +119,8 @@ const updateScore = async () => {
     const ours = scoreboards.map(scoreboard => scoreboard.find(team => team.name === teamname));
     console.log(ours);
 
+    const timestamp = new Date().toISOString();
+
     // perhaps there should be a better way to do this
     const serialized = JSON.stringify(ours);
     if (JSON.stringify(last) !== serialized) {
@@ -126,19 +128,19 @@ const updateScore = async () => {
         const changes = [];
         if (last) {
           if (result.rank < last[i].rank) {
-            changes.push(`Team ${teamname} moved up the leaderboard from **rank ${last[i].rank}** to **rank ${result.rank}**!`);
+            changes.push(`Team **${teamname.replace(/\*/g, '\\*')}** moved up the leaderboard from **rank ${last[i].rank}** to **rank ${result.rank}**!`);
           }
           if (result.rank > last[i].rank) {
-            changes.push(`Team ${teamname} moved down the leaderboard from **rank ${last[i].rank}** to **rank ${result.rank}** 😔`);
+            changes.push(`Team **${teamname.replace(/\*/g, '\\*')}** moved down the leaderboard from **rank ${last[i].rank}** to **rank ${result.rank}** 😔`);
           }
           if (result.score > last[i].score) {
-            changes.push(`Team ${teamname}'s score improved from **${last[i].score}** to **${result.score}**!`);
+            changes.push(`Team **${teamname.replace(/\*/g, '\\*')}**'s score improved from **${last[i].score}** to **${result.score}**!`);
           }
           if (result.score < last[i].score) {
-            changes.push(`Team ${teamname}'s score decreased from **${last[i].score}** to **${result.score}** ... but ... how ...?`);
+            changes.push(`Team **${teamname.replace(/\*/g, '\\*')}**'s score decreased from **${last[i].score}** to **${result.score}** ... but ... how ...?`);
           }
         } else {
-          changes.push(`Team ${teamname} is on **rank ${result.rank}** of ${scoreboards[i].length} with score **${result.score}**`);
+          changes.push(`Team **${teamname.replace(/\*/g, '\\*')}** is on **rank ${result.rank}** of ${scoreboards[i].length} with score **${result.score}**`);
         }
         if (changes.length > 0) {
           changes.push(`> Submission Date: ${result.timestamp}`);
@@ -152,7 +154,8 @@ const updateScore = async () => {
         .setColor('#008891')
         .setTitle('Rank Notification')
         .setThumbnail('https://brihackathon.id/images/logo-bri-hackathon.png')
-        .setDescription('');
+        .setDescription('')
+        .setFooter(`scoreboard fetched at ${timestamp}`);
       updates.forEach(update => {
         if (update.value) {
           message.addField(update.name, update.value);
@@ -169,21 +172,57 @@ const updateScore = async () => {
         .setColor('#008891')
         .setTitle('Top 10 Leaderboard')
         .setThumbnail('https://brihackathon.id/images/logo-bri-hackathon.png')
-        .setDescription('');
+        .setDescription('')
+        .setFooter(`scoreboard fetched at ${timestamp}`);
       contests.forEach((title, i) => {
+        // list of changes:
+        // - kicked from top 10 (disqualified or for whatever reason there is)
+        // - someone new moved to the top 10
+        // - someone already in the top 10 improved their score
+        let changes = [];
+        if (lasttop10 && lasttop10[i]) {
+          lasttop10[i].forEach(team => {
+            if (top10[i].findIndex(t => t.name === team.name) === -1) {
+              changes.push(`Team **${team.name.replace(/\*/g, '\\*')}** was out from the top 10`);
+            }
+          });
+          top10[i].forEach(team => {
+            if (lasttop10[i].findIndex(t => t.name === team.name) === -1) {
+              changes.push(`Team **${team.name.replace(/\*/g, '\\*')}** moved up to **rank ${team.rank}** with score of **${team.score}**`);
+            }
+          });
+          top10[i].forEach(team => {
+            const prev = lasttop10[i].find(t => t.name === team.name);
+            if (!prev) return;
+            if (prev.score !== team.score) {
+              if (prev.rank !== team.rank) {
+                changes.push(`Team **${team.name.replace(/\*/g, '\\*')}** improved their score from **${prev.score}** to **${team.score}** and moved to **rank ${team.rank}**`);
+              } else {
+                changes.push(`Team **${team.name.replace(/\*/g, '\\*')}** improved their score from **${prev.score}** to **${team.score}**`);
+              }
+            }
+          });
+          if (changes.length) {
+            changes = [
+              'Updates:',
+              ...changes.map(s => `> ${s}`),
+            ];
+          }
+        }
         const table = new AsciiTable();
         table.setHeading('Rank', 'Team Name', 'Score');
         top10[i].forEach((team, rank) => table.addRow(rank+1, team.name, team.score));
-        message.addField(title, ['```', table.toString(), '```'].join('\n'));
+        message.addField(title, ['```', table.toString(), '```', ...changes].join('\n'));
       });
       await notify(message);
       lasttop10 = top10;
+      console.log(lasttop10);
 
       // dumps the scoreboards
-      const timestamp = new Date().toISOString().replace(/\-/g, '').replace(/T/g, '_').replace(/\:/g, '').replace(/\.\d+Z$/g, '');
+      const escapedTimestamp = timestamp.replace(/\-/g, '').replace(/T/g, '_').replace(/\:/g, '').replace(/\.\d+Z$/g, '');
       contests.forEach(async (title, i) => {
         const csvWriter = createCsvWriter({
-          path: `dumps/scoreboard_${title.toLowerCase().replace(/ /g, '_')}_${timestamp}.csv`,
+          path: `dumps/scoreboard_${title.toLowerCase().replace(/ /g, '_')}_${escapedTimestamp}.csv`,
           header: [
             {id: 'rank', title: 'rank'},
             {id: 'name', title: 'team_name'},
